5264751bd687d7230db9e1c149a52b4dc873825f
azraculhaci <azraculhaci@hotmail.com>
2025-11-20 20:47:41 +0300

    Add entity classes and create initial EF Core migration

--- ETL_web_project/Data/Context/ProjectContext.cs
+++ ETL_web_project/Data/Context/ProjectContext.cs
@@ -1,35 +1,55 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ETL_web_project.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace ETL_web_project.Data.Context
 {
     public class ProjectContext : DbContext
     {
-        // üstteki DbContext biz databaseimizi baglayabilelim diye kalıtım aldık
-        //onu sil üstteki using kısmını da sil yine DbContext diye yaz oraya tab dediğinde o using ifadesi kendiliğinden geliyor. Burada bizim eklediğimiz paketlerden birini kullanıyoruz
-
-        //paketleri görmek icin ::  dependencies -> packages orada hepsi var
-        // paket ekleme: tools-> nuget package manager -> nuget package solution -> indirme kısmından bizim .net framework ne ise ona uyan sürümü indiriyoruz. sürümümüz 8 olduğu için 8'in son sürümünü indirdim. Ona her zaman dikkat. Zaten yanlıslıkla 10 indirirsen uyarıyor sürümler eşleşmiyor diye
-
-        //Cok önemli bir ince detay info: database tarafında tablo adı çoğul yazılır, mvc tarafında data tekil yazılıyor. Bunu tabi dikkat etmeden kullananlar da cok ama genel bi bilgi olsun buna göre eklersin ask entities sınıfına tablolarımızı
-
-
-        //MİGRATİON ALMADAN ÖNCE ENTİTYLER EKLENDİKTEN SONRA YAPILACAK KISIM BU
-
-        //   public DbSet< /*bizim entity sınıfımız*/ > /*tablodaki adlandırması*/ { get; set; }
-        
-        //Bu alttaki dısında onmodelcreating mi bir kısım daha var. Birecok cokacok falan yazdığımız. Chatgptden bi bizim tablolara göre örnek project context bakarsın ona gerek duymus mu diye.
-        
-        // ekstra alttakini nasıl actım:    override yaz, yanına onconfiguring deyip tab
-        //                                      dediğinde bu kendiliğinden oluşuyor. İçine bizim gecenki projede json dosyasında yaptığımız gibi bir sqlservera bağlantı ekledim
-        // base. kısmı silinmiyor overrideda kendiliğinden geldi sadece araya bağlantı ekliyoruz
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public ProjectContext(DbContextOptions<ProjectContext> options)
+            : base(options)
         {
-            //kendi server namelerimiz kullanılacak
-            optionsBuilder.UseSqlServer("Server=LAPTOP-BCGODK3K;Database=EtlDb;Trusted_Connection=true;TrustServerCertificate=true");
-
-            base.OnConfiguring(optionsBuilder);
         }
 
         
+        public DbSet<DimDate> DimDates { get; set; }
+        public DbSet<DimStore> DimStores { get; set; }
+        public DbSet<DimProduct> DimProducts { get; set; }
+        public DbSet<DimCustomer> DimCustomers { get; set; }
+
+        
+        public DbSet<FactSales> FactSales { get; set; }
+
+        public DbSet<SalesRaw> SalesRaws { get; set; }
+
+        
+        public DbSet<EtlJob> EtlJobs { get; set; }
+        public DbSet<EtlRun> EtlRuns { get; set; }
+        public DbSet<EtlLog> EtlLogs { get; set; }
+
+     
+        public DbSet<UserAccount> UserAccounts { get; set; }
+
+      
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+          
+            modelBuilder.Entity<DimDate>().ToTable("DimDate", schema: "dw");
+            modelBuilder.Entity<DimStore>().ToTable("DimStore", schema: "dw");
+            modelBuilder.Entity<DimProduct>().ToTable("DimProduct", schema: "dw");
+            modelBuilder.Entity<DimCustomer>().ToTable("DimCustomer", schema: "dw");
+
+            modelBuilder.Entity<FactSales>().ToTable("FactSales", schema: "dw");
+
+            modelBuilder.Entity<SalesRaw>().ToTable("SalesRaw", schema: "stg");
+
+            modelBuilder.Entity<EtlJob>().ToTable("EtlJob", schema: "etl");
+            modelBuilder.Entity<EtlRun>().ToTable("EtlRun", schema: "etl");
+            modelBuilder.Entity<EtlLog>().ToTable("EtlLog", schema: "etl");
+
+            modelBuilder.Entity<UserAccount>().ToTable("UserAccount", schema: "auth");
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
