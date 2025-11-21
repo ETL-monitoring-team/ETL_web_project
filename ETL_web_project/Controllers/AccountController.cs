d614d37afa36141067061fd23a9b931217332f3b
azraculhaci <azraculhaci@hotmail.com>
2025-11-21 12:03:36 +0300

    Implement Account auth views (Login, Register, ForgotPassword, AccessDenied) using shared auth.css styling

---
+++ ETL_web_project/Controllers/AccountController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETL_web_project.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: AccountController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AccountController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AccountController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AccountController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        // GET: AccountController/Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: AccountController/Register
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // GET: AccountController/AccessDenied
+        [HttpGet]
+        public ActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ForgotPassword(string email)
+        {
+            // Şimdilik boş bırak – ileride mail gönderme ekleyebiliriz
+            ViewBag.Message = "If the email exists, a reset link will be sent.";
+            return View();
+        }
+
+    }
+}
+
+
