4fd7119ce91806f624e72b4bd220a821333b5a75
Dila Öykü Eyüboğlu <eyubogluoyku44@gmail.com>
2025-11-27 22:59:00 +0300

    Add custom static error page and configure global exception handling. Delete home default page

--- ETL_web_project/Program.cs
+++ ETL_web_project/Program.cs
@@ -42,7 +42,7 @@ var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/error.html");
     app.UseHsts();
 }
 
